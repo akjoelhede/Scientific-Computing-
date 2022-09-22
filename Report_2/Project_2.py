c6c4bcf70b64cfb7018495b949e76c570a8838e4
akjoelhede <63365928+akjoelhede@users.noreply.github.com>
2022-09-22 16:17:15 +0200

    a

---
+++ Report_2/Project_2.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from Project_2_subroutines import *
+from example_matrices import *
+
+
+Kmat = np.load("Chladni-Kmat.npy")
+
+
+def gershgorin(A):
+	
