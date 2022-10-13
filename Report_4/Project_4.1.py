972a682c198f9a91393be49febcdac946bc1efcf
akjoelhede <63365928+akjoelhede@users.noreply.github.com>
2022-10-13 18:48:08 +0200

    h

--- Report_4/Project_4.1.py
+++ Report_4/Project_4.1.py
@@ -5,30 +5,32 @@ import matplotlib.pyplot as plt
 #A CLASS DOES NOT TAKE ANY PARAMETERS OR VALUES
 class HIV():
 
-	def __init__(self, parameters ): # THE __INIT__ FUNCTION DEFINES THE PARAMETERS IN THE CLASS
+	def __init__(self,x1, x2, y, z,parameters ): # THE __INIT__ FUNCTION DEFINES THE PARAMETERS IN THE CLASS
 		self.parameters = parameters
+		self.x1 = x1
+		self.x2 = x2
+		self.y = y
+		self.z = z
 
 		
 	#ALL FUNCTIONS BELOW THIS ARE CALLABLE 
 
-	def Diff(self,parameters):
+	def Diff(self,x1, x2, y, z, parameters):
 		
 
-		x1_new = parameters[0]*parameters[8]*(parameters[10]-parameters[8])+ parameters[1]*parameters[9]*(parameters[10]-parameters[8])
+		x1_new = parameters[0]*x1*(parameters[10]-x1)+ parameters[1]*x2*(parameters[10]-x1)
 
-		x2_new = parameters[2]*parameters[8]*(parameters[11]-parameters[9])+parameters[3]*parameters[9]*(parameters[11]-parameters[9])+ parameters[4]*parameters[12]*(parameters[11]-parameters[9])
+		x2_new = parameters[2]*x1*(parameters[11]-x2)+parameters[3]*x2*(parameters[11]-x2) + parameters[4]*y*(parameters[11]-x2)
 
-		y_new = parameters[5]*parameters[9]*(parameters[15]-parameters[12]) + parameters[6]*parameters[14]*(parameters[15]-parameters[12])
+		y_new = parameters[5]*x2*(parameters[15]-y) + parameters[6]*z*(parameters[15]-y)
 
-		z_new = parameters[7]*parameters[12]*(parameters[13]-parameters[14])
+		z_new = parameters[7]*y*(parameters[13]-z)
 
 		return x1_new, x2_new, y_new, z_new
 
 
-	def Euler(self, parameters, ):
-
-
-parameters = np.array([a1, a2, b1, b2, b3, c1, c2, d1, x1, x2, p1, p2, y, r, z, q])
+			0   1   2   3   4   5   6   7   8  9  10  11 
+parameters = np.array([a1, a2, b1, b2, b3, c1, c2, d1, p1, p2, r, q])
 
 #THIS CALLS THE CLASS AND ASSIGNS IT TO A VARIABLE S. X AND Y ARE GIVEN AS PARAMETERS
 s = HIV(parameters=parameters)
@@ -37,7 +39,7 @@ s = HIV(parameters=parameters)
 pot = s.Diff(parameters)
 
 #HERE I CALL ANOTHER FUNCTION WITHIN THE CLASS EASILY
-kin = s.kin_run(parameters)
+kin = s.Euler(parameters)
 
-print(pot, kin)
+
 
