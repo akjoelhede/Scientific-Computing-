9eb4f9f886595564519cbacd2eefb29534c2477f
akjoelhede <63365928+akjoelhede@users.noreply.github.com>
2022-10-14 10:34:51 +0200

    k

--- Report_4/Project_4.1.py
+++ Report_4/Project_4.1.py
@@ -5,41 +5,62 @@ import matplotlib.pyplot as plt
 #A CLASS DOES NOT TAKE ANY PARAMETERS OR VALUES
 class HIV():
 
-	def __init__(self,x1, x2, y, z,parameters ): # THE __INIT__ FUNCTION DEFINES THE PARAMETERS IN THE CLASS
+	def __init__(self,parameters ): # THE __INIT__ FUNCTION DEFINES THE PARAMETERS IN THE CLASS
 		self.parameters = parameters
-		self.x1 = x1
-		self.x2 = x2
-		self.y = y
-		self.z = z
 
 		
 	#ALL FUNCTIONS BELOW THIS ARE CALLABLE 
 
-	def Diff(self,x1, x2, y, z, parameters):
+	def Diff(self,parameters):
 		
+		x1, x2, y, z, a1, a2, b1, b2, b3, c1, c2, d1, p1, p2, r, q = parameters
 
-		x1_new = parameters[0]*x1*(parameters[10]-x1)+ parameters[1]*x2*(parameters[10]-x1)
+		diffx1 = a1*x1*(p1-x1)+ a2*x2*(p1-x1)
 
-		x2_new = parameters[2]*x1*(parameters[11]-x2)+parameters[3]*x2*(parameters[11]-x2) + parameters[4]*y*(parameters[11]-x2)
+		diffx2 = b1*x1*(p2-x2)+b2*x2*(p2-x2) + b3*y*(p2-x2)
 
-		y_new = parameters[5]*x2*(parameters[15]-y) + parameters[6]*z*(parameters[15]-y)
+		diffy = c1*x2*(q-y) + c2*z*(q-y)
 
-		z_new = parameters[7]*y*(parameters[13]-z)
+		diffz = d1*y*(r-z)
 
-		return x1_new, x2_new, y_new, z_new
+		return diffx1, diffx2, diffy, diffz
 
+	
+	def euler(self, x0, diff, dt):
 
-			0   1   2   3   4   5   6   7   8  9  10  11 
-parameters = np.array([a1, a2, b1, b2, b3, c1, c2, d1, p1, p2, r, q])
+		x_new = x0 + diff * dt
+
+		return x_new
+x1 = 0.01
+x2 = 0
+y = 0
+z = 0
+
+a1 = 10
+a2 = 5
+b1 = 5
+b2 = 0
+b3 = 0
+c1 = 0
+c2 = 0
+d1 = 0
+p1 = 5
+p2 = 5
+r = 100 
+q = 100 
+
+parameters = np.array([x1, x2, y, z, a1, a2, b1, b2, b3, c1, c2, d1, p1, p2, r, q])
 
 #THIS CALLS THE CLASS AND ASSIGNS IT TO A VARIABLE S. X AND Y ARE GIVEN AS PARAMETERS
 s = HIV(parameters=parameters)
 
 #HERE I CALL A FUNCTION WITHIN THE CLASS TO CALCULATE THE POTENTIAL ENERGY
-pot = s.Diff(parameters)
+diffx1, diffx2, diffy, diffz = s.Diff(parameters)
 
 #HERE I CALL ANOTHER FUNCTION WITHIN THE CLASS EASILY
-kin = s.Euler(parameters)
+new_step = s.euler(x1, diffx1, 0.1)
+print(new_step)
+
 
 
 
